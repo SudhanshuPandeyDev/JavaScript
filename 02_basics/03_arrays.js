b99e6614bdd47385d735918bc6dd651297e7ffc7
Sudhanshu Pandey <154343364+SudhanshuPandeyDev@users.noreply.github.com>
2024-08-28 08:47:07 +0000

    Arrays

--- 02_basics/03_arrays.js
+++ 02_basics/03_arrays.js
@@ -16,3 +16,12 @@ const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 // how you are going to handle array like this...using flat method
 const real_another_array = another_array.flat(Infinity)
 console.log(real_another_array)
+
+console.log(Array.isArray("sudhanshu"))     // check if it is array or not
+console.log(Array.from("pandey"))       // The Array.from() method creates a new array from an iterable or array-like object.
+console.log(Array.from({Name : "sudhanshu"}))      // interesting case shows empty array
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+console.log(Array.of(score1,score2,score3))
