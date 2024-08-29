d95d43d2e7294796e8a66dc52b441996ed9b110d
Sudhanshu Pandey <154343364+SudhanshuPandeyDev@users.noreply.github.com>
2024-08-29 06:58:14 +0000

    scope

--- 03_basics/03_scope.js
+++ 03_basics/03_scope.js
@@ -8,3 +8,36 @@ if(true){
 }
 console.log(a)
 // console.log(c)
+
+
+function one(){
+    const username = "hitesh"
+    function two(){
+        const website = "youtube"
+        console.log(username)     // child function can access parent variables
+    }
+    // console.log(website)       
+    two()
+}
+one()
+
+if(true){
+    const username = "sudhanshu"
+    if(username == "sudhanshu"){
+        const website = " youtube"
+        console.log(username + website)
+    }
+    // console.log(website)     error cannot access webiste
+}
+
+
+// --------------------------- two different ways to make functions---------------------
+console.log(addOne(5))
+function addOne(num){
+    return num + 1
+}
+
+// console.log(addTwo(5))      // shows error cannot access addTwo before initialization
+const addTwo = function(num){
+    return num + 2
+}
