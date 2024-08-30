b7980b0fdf2050d9866f056e06cf9387dc6de419
Sudhanshu Pandey <154343364+SudhanshuPandeyDev@users.noreply.github.com>
2024-08-30 11:40:00 +0000

    filter in js

---
+++ 05_iterations.js/08_filter.js
@@ -0,0 +1,24 @@
+// filter return us some values but forEach method does not return any values
+// Use Case: Use forEach when you want to perform an action for each element in the array, without needing to create a new array.
+// Use Case: Use filter when you need a new array based on certain criteria.
+
+const myNums = [1,2,3,4,5,6,7,8,9]
+
+// basic arrow function (explicit return)
+const newNums1 = myNums.filter( (num) => {
+    return num > 4
+} )
+console.log(newNums1)
+
+// implicit return
+const newNums2 = myNums.filter( (num) => num > 4 )
+console.log(newNums2)
+
+// we can also do this thing using forEach loop (both filter and this method are equally efficient)
+const newNums3 = []
+myNums.forEach( (num) => {
+    if(num > 4){
+        newNums3.push(num)
+    }
+})
+console.log(newNums3)
