184c0859d44677fb1742ee6619f043b115f0b42f
Sudhanshu Pandey <154343364+SudhanshuPandeyDev@users.noreply.github.com>
2024-08-30 02:58:59 +0000

    Ternary Operator

---
+++ 04_control_flow/05_ternary operator.js
@@ -0,0 +1,7 @@
+// condition ? expressionIfTrue : expressionIfFalse;
+
+const iceTeaPrice = 90
+iceTeaPrice<80 ? console.log("lesser than 80") : console.log("greater than 80")
+
+// strict equality operators --> === and !==
+// loose equality operators --> == and !=
