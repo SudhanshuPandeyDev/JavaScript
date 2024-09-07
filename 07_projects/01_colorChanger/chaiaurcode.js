068d34fe34aaada76ac0fe39cbdeecf7842b8bf8
Sudhanshu Pandey <154343364+SudhanshuPandeyDev@users.noreply.github.com>
2024-09-07 03:48:35 +0000

    colorChanger Project

---
+++ 07_projects/01_colorChanger/chaiaurcode.js
@@ -0,0 +1,24 @@
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+buttons.forEach(function (button) {
+  console.log(button);
+  button.addEventListener('click', function (e) {
+    // The e.target will print the <span class=" " id=" "></span> element in the console, indicating that it was the element that received the click event.
+    console.log(e.target);          // This logs the DOM element that triggered the event, which in this case, is the clicked button.
+    if (e.target.id === 'grey') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'white') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'blue') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'yellow') {
+      body.style.backgroundColor = e.target.id;
+    }
+    
+  });
+});
+
